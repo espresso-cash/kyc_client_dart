b59c90b2324d108c5fb0ba7bafaccdd1cd62a3c4
Vlad Sumin <vladsum00@gmail.com>
2024-10-18 18:33:19 +0300

    upd

--- lib/src/partner_client.dart
+++ lib/src/partner_client.dart
@@ -12,6 +12,7 @@ import 'package:kyc_client_dart/src/common.dart';
 import 'package:pinenacl/ed25519.dart';
 import 'package:pinenacl/tweetnacl.dart';
 import 'package:pinenacl/x25519.dart';
+import 'package:uuid/uuid.dart';
 
 class KycPartnerClient {
   KycPartnerClient({
@@ -137,13 +138,12 @@ class KycPartnerClient {
       secretBox: SecretBox(Uint8List.fromList(base58.decode(secretKey))),
       signingKey: _signingKey,
     );
-
     await _kycClient.kycServiceSetValidationData(
       body: V1SetValidationDataRequest(
         encryptedData: base64Encode(encryptedData),
         userPublicKey: userPK,
         dataId: value.dataId,
-        id: '',
+        id: const Uuid().v4(),
       ),
     );
   }
